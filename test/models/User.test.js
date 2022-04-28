5aecedeb1f914b40a3fc0d3c45b7f518e0703e53
JossPerezT <jossperez44@gmail.com>
2022-04-27 23:11:50 -0500

    9. Modificando la prueba para que no se envíen los valores dateCreated y lastUpdated

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,13 +1,13 @@
 const User = require("./../../app/models/User")
 describe ("TDD para User class", () =>{
     test (`1. Crear un objeto para el ussuario`, () => {
-        const user_Joss = new User (1, "jocelynperez", "Jocelyn", "bio", "dateCreated", "lastUpdated")
+        const user_Joss = new User (1, "jocelynperez", "Jocelyn", "bio")
 
         expect (user_Joss.id).toBe(1)
         expect(user_Joss.userName).toBe("jocelynperez")
         expect(user_Joss.name).toBe("Jocelyn")
         expect(user_Joss.bio).toBe("bio")
-        expect(user_Joss.dateCreated).toBe("dateCreated")
-        expect(user_Joss.lastUpdated).toBe("lastUpdated")
+        expect(user_Joss.dateCreated).not.toBeUndefined()
+        expect(user_Joss.lastUpdated).not.toBeUndefined()
     })
 })
