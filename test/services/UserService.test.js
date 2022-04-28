0f0e5906d9b837e9a936852d2e8ab07e52d36ba8
JossPerezT <jossperez44@gmail.com>
2022-04-28 16:38:12 -0500

    7. Agregando el código para la prueba de unidad del requerimiento 2. para que regrese los valores de unidad en una lista

--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -8,4 +8,12 @@ describe ('1. Test para UserService', ()=>{
         expect(user.id).toBe(1)
         expect(user.bio).toBeDefined()
     })
+    test ('Requerimiento 2: getInfo que regrese los valores de un objeto', () =>{
+        const user = UserService.create (1, "jocelynperez", "Jocelyn")
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe(1)
+        expect(userInfoList[1]).toBe("jocelynperez")
+        expect(userInfoList[2]).toBe("Jocelyn")
+        expect(userInfoList[3]).toBe("Sin bio")
+    })
 })
