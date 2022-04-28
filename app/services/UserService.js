bf6cc8ab0ee42c345425468f15cc8742cef6d2e2
JossPerezT <jossperez44@gmail.com>
2022-04-28 16:47:59 -0500

    8. Agregando el código del requerimiento 2. spring 2. Semana 3. La prueba pasó

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,11 +4,13 @@ class UserService {
     static create (id, userName, name){
         return new User(id, userName, name,"Sin bio")
     }
+    static getInfo(id, userName, name, bio, dateCreated, lastUpdated){
+        return Object.values(id, userName, name, bio, dateCreated, lastUpdated)
+
+    }
 }
 
 
 module.exports = UserService
 
-const user = UserService.create (1, "jocelynperez", "Jocelyn")
 
-console.log (user)
