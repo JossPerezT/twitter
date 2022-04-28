b1af2920845da32bfcb676d1fdb391910fc1e42c
JossPerezT <jossperez44@gmail.com>
2022-04-28 14:17:42 -0500

    2. Agregando la prueba de unidad del requerimiento 1, ejercicio sprint 2

---
+++ test/services/UserService.test.js
@@ -0,0 +1,11 @@
+const UserService = require('../../app/services/UserService')
+
+describe ('1. Test para UserService', ()=>{
+    test ('Requerimiento 1. Crear un nuevo usuario',() => {
+        const user = UserService.create (1, "jocelynperez", "Jocelyn")
+        expect(user.userName).toBe("jocelynperez")
+        expect(user.name).toBe("Jocelyn")
+        expect(user.id).toBe(1)
+        expect(user.bio).not.toBeUndefined()
+    })
+})
