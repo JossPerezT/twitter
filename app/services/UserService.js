55b5b7c350447d8944c02d59b39f98e519788225
JossPerezT <jossperez44@gmail.com>
2022-04-28 21:00:34 -0500

    Corrigiendo a una mejor práctica el requerimiento 2 de sprint 2

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -4,8 +4,8 @@ class UserService {
     static create (id, userName, name){
         return new User(id, userName, name,"Sin bio")
     }
-    static getInfo(id, userName, name, bio, dateCreated, lastUpdated){
-        return Object.values(id, userName, name, bio, dateCreated, lastUpdated)
+    static getInfo(user){
+        return Object.values(user)
 
     }
     static updateUserUserName (user, userName){
