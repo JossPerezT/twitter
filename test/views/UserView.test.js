eaabba6bba17a0ed498c9434afb126b74b4b0dfc
JossPerezT <jossperez44@gmail.com>
2022-04-28 22:22:38 -0500

    4. Agregando el código de la prueba del requerimiento 1 sprint 3: Al tener un valor null se obtenga un error específico

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -1,2 +1,9 @@
 const UserView = require('../../app/views/UserView')
 
+describe ('Tests para UserView', () =>{
+    test('Requerimiento 1: Al tener un valor null, se obtenga un error', () =>{
+        const payload = null 
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/payload no existe/)
+    })
+})
