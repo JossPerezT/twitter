2e4514d68954dc3b758bc328490eaac9d3a0f57c
JossPerezT <jossperez44@gmail.com>
2022-05-01 14:29:49 -0500

    8. Modificando la prueba para que pasé

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -2,7 +2,7 @@ const UserView = require('../../app/views/UserView')
 
 describe ('Tests para UserView', () =>{
     test('Requerimiento 1: Al tener un valor null, se obtenga un error', () =>{
-        const payload = nul 
+        const payload = null
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/payload no existe/)
     })
